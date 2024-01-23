7c234d229f444006ec9086c4dcfbba917795a551
mctola1 <119270609+mctola1@users.noreply.github.com>
2024-01-23 09:48:07 +0100

    first

---
+++ js/input.js
@@ -0,0 +1,12 @@
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+
+let bonusPoints = 50
+
+count = count + 50
+
+count = count - 75
+
+count = count + 45
+
+console.log(count)
